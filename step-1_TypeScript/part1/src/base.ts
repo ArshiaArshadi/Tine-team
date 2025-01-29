ac59b3b4f0d1ad1b6d36a89c071f81f31e29219a
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2025-01-29 17:21:04 +0330

    feat : add type
    array & Tuple

--- step-1_TypeScript/part1/src/base.ts
+++ step-1_TypeScript/part1/src/base.ts
@@ -1,1 +1,20 @@
 let id:number = 4
+let company:string = 'Google'
+let isPublished:boolean = true
+let x:any = 'hello'
+let age:number
+age = 5
+// number array
+let ids: number[] = [1,4,6,7,8]
+ids.push(5)
+let arr : any[] = [1,true,'hello']
+
+// Tuple
+let person :[number,string,boolean] = [1,'the person',true]
+// Tuple Array
+let employee: [number,string][]
+employee = [
+    [1,'rt'],
+    [5,'D'],
+    [4,'SD']
+]

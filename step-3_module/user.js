8470a9369588575f42b33d13222f7353314a9664
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2025-02-02 12:47:21 +0330

    build : module file
    create file and test code

---
+++ step-3_module/user.js
@@ -0,0 +1,15 @@
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+function printname(user) {
+  console.log(`User's name is ${user.name}`);
+}
+
+function printname(age) {
+  console.log(`User is ${user.age} years old`);
+}
+export
